f5cae34133ad912a1cb327d893020da95c44e074
Jan Pink <jan.pink@fds-limburg.schule>
2022-11-11 08:55:47 +0100

    v1 finished

--- umr_le.js
+++ umr_le.js
@@ -1,5 +1,27 @@
-
-function convert() {
+function selectState() {
+    var endState = document.getElementById('targetUnitID').value
+    var selState = document.getElementById('sourceUnitID').value
     var inputID = document.getElementById("inputID").value
-    alert("my cock is " + inputID +" cm long")
+    var outputID = document.getElementById('resultID').innerHTML = outputID
+    if (endState == selState) {
+        alert("you cant convert into the same unit")
+    }
+    else {
+        console.log("current value: " + inputID +" current state: " + selState + " // wanted value: " + endState)
+        if (selState == "m") {
+            var result = (inputID * 1000000000000)
+        }
+        else if (selState == "pm") {
+            var result = (inputID * 1.E-12)
+        }
+
+        if (result > 1 && endState =="m") {
+            result = Math.round(result)
+        }
+        outputID = ("result: " + result + " " + endState)
+        document.getElementById('resultID').innerHTML = outputID
+        console.log(outputID)
+    }
+   
+
 }
